632d246ae1d1fea4e83e0c0b83f437c90d962c73
Karthik Ramesan <136253498+fsydurden@users.noreply.github.com>
2025-06-05 20:49:30 +0530

    Update main.py
    made the code more flexible and re-usable

--- main.py
+++ main.py
@@ -1,46 +1,60 @@
 import random
 from art import logo
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+print(logo)
 
-strat_game = str(input("Do you want to play the black Jack game? Type y for yes and n for no."))
-strat_game.lower()
-if strat_game == "y":
-    print(logo)
-    user_cards = [random.choice(cards) , random.choice(cards)]
-    computer_choice = [random.choice(cards), random.choice(cards)]
-    print(f"Your cards :{user_cards}")
-    print(f"Dealers cards: {computer_choice[0]}")
-    user_total = user_cards[0] + user_cards[1]
-    computer_total = computer_choice[0] + computer_choice[1]
-    stage_two = str(input("Type y for yes if you want to take another card"))
-    if stage_two == "y":
-        user_cards += [random.choice(cards)]
-        user_total += user_cards[2]
-        print(f"Your cards : {user_cards} and your total score : {user_total}")
-        if user_total > 21:
-            print("You loose, your total is more than 21")
-        else:
-            stage_three = str(input("Type y for yes if you want to take another card"))
-            if stage_three == "y":
-                user_cards += [random.choice(cards)]
-                user_total += user_cards[3]
-                if user_total > 21:
-                    print("You loose, your total is more than 21")
-                else:
-                    computer_choice += [random.choice(cards)]
-                    computer_total += computer_choice[2]
-                    if computer_total > 21:
-                        print("User wins")
-                    elif computer_total == user_total:
-                        print("Its a draw!!")
-                    else:
-                        print("Computer wins")
+def deal_cards():
+    """Function that picks cards at random"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    """This loop assigns two random cards each to user and computer """
+    user_cards.append(deal_cards())
+    computer_cards.append(deal_cards())
+
+print(user_cards)
+print(computer_cards)
+def calculate_score(card_list):
+    """Function that calculates scores in the list"""
+    total = sum(card_list)
+    if total == 21:
+        return 0
+    elif total > 21 and 11 in card_list:
+            card_list.remove(11)
+            card_list.append(1)
+            total = sum(card_list)
+            return total
     else:
-        if user_total < computer_total:
-            print("You wins")
-        elif user_total > computer_total:
-            print("Computer wins")
-        elif user_total == computer_total:
-            print("Its a draw!!!!!!1")
-else:
-    print("Game ended!")
+        return total
+
+game_is_on = True
+
+def check_winner(user_card_list, computer_card_list):
+    user_total = calculate_score(user_card_list)
+    computer_total = calculate_score(computer_card_list)
+    if user_total == 0:
+        return "You won!! (Blackjack)"
+    elif computer_total == 0:
+        return "You lose, computer has Blackjack!!"
+    elif user_total > 21:
+        return "You lose!! Your score exceeds 21."
+    elif computer_total > 21:
+        return "You won!! Computer score exceeds 21."
+    else:
+        return True
+
+winner = check_winner(user_cards,computer_cards)
+print(winner)
+
+while winner:
+    choice = str(input("type 'y' if you want another card")).lower()
+    if choice == "y":
+        user_cards.append(deal_cards())
+        print(user_cards)
+        winner = check_winner(user_cards, computer_cards)
+    else:
+        print("Game ended")
